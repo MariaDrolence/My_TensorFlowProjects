3c5404eb9d87e5100892595677feaeaee94e708c
Maria Drolence <33215987+MariaDrolence@users.noreply.github.com>
2026-01-02 16:37:17 +0000

    solving overfitting with early stop
    import earlystopping form tensorflow.keras.callback
    fit the data as usual but add one additional parameter 'earlyStopping' and define corresponding parameters such as mode, monitor,verbose and patience
    compute the loss
    plot the loss
    notice that the validation loss stabilised

--- cancer_Data_Exploration.py
+++ cancer_Data_Exploration.py
@@ -61,8 +61,47 @@ model.add(Dense(15, activation='relu'))
 # Output layer-for binary classification the activation function is sigmoid
 model.add(Dense(1, activation='sigmoid'))
 
+# Compile the model--for binary classification the loss function is binary_crossentropy--optimizer can be adam.
+# note that compiling means configuring the model for training
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+# Train the model using the fit method
+#model.fit(x=X_train, y=y_train, epochs=600, validation_data=(X_test, y_test))
+
+#losses=pd.DataFrame(model.history.history)
+#plot the losses
+#when the validation loss starts increasing while the training loss is decreasing, it indicates overfitting to the training data.
+#plt.figure(figsize=(14,12))
+#losses.plot()
+#plt.savefig('C:/Users/Maria/Documents/GitHub/TensorflowProjects/cancer_model_loss_accuracy.png')    
 
 
+#to solve overfitting, we can use callbacks such as early stopping or dropout layers during training.
+
+from tensorflow.keras.callbacks import EarlyStopping
+early_stop=EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=25)
+
+# Train the model using the fit method- with early stopping which stops training when the validation loss does not improve for 25 consecutive epochs
+model.fit(x=X_train, y=y_train, epochs=600, validation_data=(X_test, y_test), callbacks=[early_stop])
+
+#plot the losses again to see the effect of early stopping
+plt.figure(figsize=(14,12))
+model_loss=pd.DataFrame(model.history.history)  
+
+model_loss.plot()
+plt.savefig('C:/Users/Maria/Documents/GitHub/TensorflowProjects/cancer_model_loss_accuracy_early_stopping.png')
+
+#another way to solve overfitting is to use dropout layers in the model architecture.
+from tensorflow.keras.Layers import Dropout
+
+model_dropout=Sequential()
+model_dropout.add(Dense(30, activation='relu'))
+model_dropout.add(Dropout(0.5))  # Dropout layer with 50% dropout rate
+model_dropout.add(Dense(15, activation='relu'))
+model_dropout.add(Dropout(0.5))  # Dropout layer with 50% dropout rate
+model_dropout.add(Dense(1, activation='sigmoid'))
+
+model_dropout.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model_dropout.fit(x=X_train, y=y_train, epochs=600, validation_data=(X_test, y_test), callbacks=[early_stop])
 
