a2f4ae7c89dc7e3a02a3385d4ada3794a4a98789
Maria Drolence <33215987+MariaDrolence@users.noreply.github.com>
2026-01-02 00:17:52 +0000

    Add files via upload

--- House_Price_Pred.py
+++ House_Price_Pred.py
@@ -6,6 +6,8 @@ import tensorflow as tf
 
 import matplotlib.pyplot as plt
 
+import seaborn as sns
+
 #load the dataset
 df_house=pd.read_csv('C:/Users/Maria/Documents/GitHub/TensorflowProjects/TensorFlow_FILES/TensorFlow_FILES/DATA/kc_house_data.csv')
 
@@ -14,6 +16,25 @@ print(df_house.head())
 
 #view the summary information about the dataset
 print(df_house.info())
+#from the infor we don't have any missing values
+
+#this can also be checked using isnull().sum()
+print(df_house.isnull().sum())
+
+#view the statistical summary of the dataset-such as mean, std, min, max, and percentiles
+print(df_house.describe())
+
+#visualize the distribution of the target variable 'price'
+plt.figure(figsize=(10,6))
+sns.displot(df_house['price'])
+#plt.title('Distribution of House Prices')
+#plt.xlabel('Price')
+#plt.ylabel('Frequency')
+plt.show()
+#plt.savefig('house_price_distribution.png')
+
+sns.countplot(x='bedrooms', data=df_house)
+plt.show()
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
