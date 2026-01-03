36b1ad9e69d0dffa6da0336aa918f521bf4f3d8a
Maria Drolence <33215987+MariaDrolence@users.noreply.github.com>
2026-01-03 18:22:41 +0000

    Preparing my data
    Reading my data into python

---
+++ pay_back_loan.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df_loan=pd.read_csv('C:/Users/Maria/Documents/GitHub/TensorflowProjects/TensorFlow_FILES/TensorFlow_FILES/DATA/lending_club_loan_two.csv')
+
+print(df_loan.head())
