8d4a54a3fd41983b606cbf5139730ce74e638f13
Maria Drolence <33215987+MariaDrolence@users.noreply.github.com>
2026-01-03 18:32:34 +0000

    checking for columns with null values
    from the df_loan.info() we have columns with missing values.
    Now we have to proceed to check the missing values in each column using
    df_loan.isnull().sum()
    
    #noticed that the columns with missing value are:
    #emp_title - 22927
    #emp_length - 18301
    #title - 1756
    #mort_acc - 37795
    #pub_rec_bankruptcies - 535
    #revol_util - 276

--- pay_back_loan.py
+++ pay_back_loan.py
@@ -6,4 +6,20 @@ import matplotlib.pyplot as plt
 
 df_loan=pd.read_csv('C:/Users/Maria/Documents/GitHub/TensorflowProjects/TensorFlow_FILES/TensorFlow_FILES/DATA/lending_club_loan_two.csv')
 
-print(df_loan.head())
+#print(df_loan.head())
+
+print(df_loan.info())
+#from the info we can see that there are some null values in the columns
+
+#checking the null values in each column
+print(df_loan.isnull().sum())
+#noticed that the columns with missing value are:
+df_loan.isnull().sum()
+
+#noticed that the columns with missing value are:
+#emp_title - 22927
+#emp_length - 18301
+#title - 1756
+#mort_acc - 37795
+#pub_rec_bankruptcies - 535
+#revol_util - 276
